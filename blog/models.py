7bd0ba97c9a9087430c092c920b733b4e23c0148
omid <techwithomid@gmail.com>
2021-07-03 20:23:18 +0430

    create base model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=256)
+    thumbnail = models.ImageField(
+        upload_to='uploads/%Y/%m/%d/', null=True, blank=True)
+    content = models.TextField()
+    gategory = models.ManyToManyField(Category)
+    date = models.DateTimeField(default=timezone.now)
