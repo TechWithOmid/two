a7af54a07fa0d799a6a619f103752deb03191892
omid <techwithomid@gmail.com>
2021-11-07 21:33:22 +0330

    delete static files and rgnize css asset file

--- two/settings.py
+++ two/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
-    BASE_DIR / "static"
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # CKEditor settings
