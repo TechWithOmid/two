01072a9daf4f20502cccd69cbebcfd78ffceb803
omid <techwithomid@gmail.com>
2021-11-09 23:04:13 +0330

    change static file place

--- two/settings.py
+++ two/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
     os.path.join(BASE_DIR, 'static'),
 ]
