0632e2892ee2761c67a8a8b9757d858717d8fb8d
omid <techwithomid@gmail.com>
2022-01-10 19:55:47 +0330

    fix ALLOWED_HOST most define

---
+++ two/settings/__init__.py
@@ -0,0 +1,3 @@
+from .base import *
+
+ALLOWED_HOSTS = ['www.techwtihomid.ir', 'techwithomid.ir']
--- two/settings/base.py
+++ two/settings/base.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from dotenv import load_dotenv, find_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,9 +9,6 @@ SECRET_KEY = "django-insecure-3*j%!n*4#@y7#f3533w5&o*5n7i1hp(#^qcm#$9t956#igcik&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
-ALLOWED_HOSTS = ["techwithomid.ir", "www.techwithomid.ir"]
-
 
 # Application definition
 
