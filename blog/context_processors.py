1d6914aca17a33083a0118313afaea222ef0cdaa
omid <omidmmadi@gmail.com>
2022-01-28 15:36:34 -0500

    fix ObjectDoesNotExists after running the project

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -1,9 +1,13 @@
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Writer
 
 
 def writer_info(request):
     """ Create context processor for send writer_info.bio 
-     to every view for showing in side bar """
-    writer_info = Writer.objects.get(pk=1)
-    return { "writer_info": writer_info }
+    to every view for showing in side bar """
+    try:
+        writer_info = Writer.objects.get(pk=1)
+        return { "writer_info": writer_info }
+    except ObjectDoesNotExist:
+        return { "writer_info": "Nothing" }
 
